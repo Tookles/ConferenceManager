4e74bd8b1cbcf89038ba4531affdff30124bff18
Tookey <rachel.tookey@gmail.com>
2025-01-07 10:38:16 +0000

    Created Health check get endpoint

---
+++ ConferenceManager/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManager.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase 
+    {
+        [HttpGet]
+        public IActionResult CheckHealth()
+        {
+            return Ok("Server is responding");
+        }
+    }
+}
